9702da839c0a0df021a5bc2464f91050f43f5a11
amararestuamara <68101697+amararestuamara@users.noreply.github.com>
2020-07-16 12:28:06 +0700

    Add files via upload

---
+++ reservasi.py
@@ -0,0 +1,53 @@
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+reservasi_db = []
+
+
+@app.route('/jadwalReservasi', methods=['GET'])
+def tampil_reservasi():
+    nama_dokter = []
+    klinik = []
+    hari = []
+    jam_praktek = []
+    for x in reservasi_db:
+        nama_dokter.append(x[0])
+        klinik.append(x[1])
+        hari.append(x[2])
+        jam_praktek.append(x[3])
+
+    return jsonify({
+        "chats": [
+            {
+                "text": "Jadwal reservasi \n\nNama Dokter : " + ", ".join(nama_dokter),
+                "type": "text"
+            },
+            {
+                "text": "Hari : " + ", ".join(hari),
+                "type": "text"
+            },
+             {
+                "text": "Pukul : " + ", ".join(jam_praktek),
+                "type": "text"
+            }
+        ]
+    })
+
+
+@app.route('/jadwalReservasi', methods=['POST'])
+def tambah_reservasi():
+    data = request.get_json()
+    reservasi_db.append([data["nama_dokter"], data["hari"], data["jam_praktek"]])
+
+    return jsonify({
+        "chats": [
+            {
+                "text": "Data berhasil disimpan",
+                "type": "text"
+            }
+        ]
+    })
+
+
+if __name__ == '__main__':
+    app.run(port=5000)
